43b2f6f9fdd3038fd0168a022ac7d701a64c0df5
bigdoodr <37274306+bigdoodr@users.noreply.github.com>
2026-05-07 19:45:34 -0500

    Initial Commit

---
+++ PomoTimer/PomoTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoTimerApp.swift
+//  PomoTimer
+//
+//  Created by Casey Scruggs on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct PomoTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
